89570c42c29eb147e478205138f788db9a1c10f9
jljianglin <861711433@qq.com>
2017-08-04 18:30:09 +0800

    Initial commit

---
+++ src/main/java/TestHelloWord.java
@@ -0,0 +1,6 @@
+/**
+ * Created by 刘畅 on 2017/8/4.
+ */
+public class TestHelloWord {
+
+}
---
+++ src/test/java/TestHelloWord.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+/**
+ * Created by 刘畅 on 2017/8/4.
+ */
+public class TestHelloWord {
+    @Test
+    public void jiang(){
+        System.out.println("怎么出不来啊?");
+    }
+    @Test
+    public void SayHelloWord(){
+        System.out.println("啥玩意儿啊");
+
+
+    }
+
+
+
+}
